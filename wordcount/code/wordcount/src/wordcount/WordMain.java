5343d449fded92838475d36dcc35a07cd507d197
pangjiuzala <132553876@qq.com>
2015-12-01 18:40:02 +0800

    first commit

---
+++ wordcount/code/wordcount/src/wordcount/WordMain.java
@@ -0,0 +1,35 @@
+package wordcount;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class WordMain {
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+
+		conf.set("mapred.job.tracker", "127.0.0.1:9001");
+		String[] ars = new String[] { "input", "output" };
+		String[] otherArgs = new GenericOptionsParser(conf, ars)
+				.getRemainingArgs();
+		if (otherArgs.length != 2) {
+			System.err.println("Usage: wordcount <in> <out>");
+			System.exit(2);
+		}
+		Job job = new Job(conf, "word count");
+		job.setJarByClass(WordMain.class);
+		job.setMapperClass(WordMapper.class);
+		job.setCombinerClass(WordReducer.class);
+		job.setReducerClass(WordReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+
+}
